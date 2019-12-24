0d2a8284d11e9bb3ce8cf94a6cfe0bfb44e00335
Pavel Dekhman <paveldekhman@Pavels-MacBook-Air.local>
2019-12-24 16:17:12 -0500

    Add make data and build features files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,59 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import os
+pd.set_option('display.max_columns', 999)
+pd.set_option('display.max_rows', 999)
+from datetime import date
+
+def load_clean(player,dfs):
+       #change player column names
+       player.columns = ['DATASET', 'GAME_ID', 'DATE', 'PLAYER_ID', 'NAME', \
+              'POSITION', 'TEAM', 'OPP', 'VENUE', \
+              'START', 'MIN', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA', 'OR', \
+              'DR', 'TOT', 'A', 'PF', 'ST', 'TO', 'BL', 'PTS', 'USAGE', \
+              'REST']
+
+       #clean dfs file
+       keep_cols = [1,3,13,16]
+       dfs  = dfs.iloc[:,keep_cols].copy()
+       dfs.columns = dfs.iloc[0,:]
+       dfs.drop([0],axis=0,inplace=True)
+       dfs.columns = ['GAME_ID','PLAYER_ID','POS','SALARY']
+       dfs['GAME_ID'] = dfs.GAME_ID.astype(int)
+       dfs['PLAYER_ID'] = dfs.PLAYER_ID.astype(int)
+
+       #merge player and dfs files
+       df = player.merge(dfs,how = 'left',on = ['GAME_ID','PLAYER_ID'])
+
+       #clean merged file
+       df.dropna(inplace=True)
+       df.reset_index(drop=True,inplace=True)
+       df.columns  = [i.lower() for i in df.columns]
+       df.drop(['position'],axis=1,inplace=True)
+       df['salary'] = df.salary.astype(int)
+
+       #reorder columns
+       cols = list(df.columns.values) 
+       cols.pop(cols.index('rest')) 
+       cols.pop(cols.index('pos')) 
+       df = df[['rest','pos']+ cols] 
+
+       return df
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+if __name__ == "__main__":
+       #set raw and interim data paths
+       raw_data_dir = '~/nba_dfs_value/data/raw/'
+       interim_data_dir= '~/nba_dfs_value/data/interim/'
+       processed_data_dir = '~/nba_dfs_value/data/processed/'
 
+       #read player file
+       player_file = raw_data_dir+'NBA1819.csv'
+       player= pd.read_csv(player_file,parse_dates = ['DATE'])
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+       #read dfs file
+       dfs_file = raw_data_dir + 'DFS1819.csv'
+       dfs = pd.read_csv(dfs_file)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+       load_clean(player,dfs)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+       df.to_pickle(interim_data_dir+'clean_merge.pkl')
---
+++ src/features/build_features.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+
+
+def build_features(ndf):
+    #create FP and value columns 
+    ndf['fp_pts'] = ndf['pts']*1
+    ndf['fp_fg'] = ndf['fg']*2
+    ndf['fp_3p'] = ndf['3p']*3
+    ndf['fp_ft'] = ndf['ft']*1
+    ndf['fp_or'] = ndf['or']*1.2
+    ndf['fp_dr'] = ndf['dr']*1.2
+    ndf['fp_tot'] = ndf['tot']*1.2
+    ndf['fp_a'] = ndf['a']*1.5
+    ndf['fp_st'] = ndf['st']*3
+    ndf['fp_to'] = ndf['to']*-1
+    ndf['fp_bl'] = ndf['bl']*3
+    ndf['fp'] = ndf.fp_pts+ndf.fp_tot+ndf.fp_a+ndf.fp_st+ndf.fp_to+ndf.fp_bl
+    ndf['fpd'] = ndf.fp*1000/ndf.salary
+    ndf['value'] = np.where(ndf['fpd']>5, 1, 0)
+    ndf['rest'] = np.where(ndf['rest']=='3+',99,ndf['rest'])
+    ndf['rest'] = pd.to_numeric(ndf['rest'])
+    ndf['rest'] = np.where(ndf['rest']>3,'3+',ndf['rest'])
+
+    #set up per min columns
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min')+1:].columns
+    ndf[[f"{i}_per_min" for i in ncols]] = ndf[ncols].transform(lambda x : x/ndf['min'])
+
+    #set up fp% columns
+    ncols = ndf.iloc[:,ndf.columns.get_loc('fp_pts'):ndf.columns.get_loc('fp')].columns
+    ndf[[f"{i}_%_fp" for i in ncols]] = ndf[ncols].transform(lambda x : x/ndf['fp'])
+
+    # 3 game moving average 
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min'):].columns
+    ndf[[f"{i}_3MA" for i in ncols]] = ndf.groupby('player_id') \
+                        [ncols].transform(lambda x : x.rolling(window=3,min_periods=1).mean())
+
+    #5 game moving average
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min'):].columns
+    ndf[[f"{i}_5MA" for i in ncols]] = ndf.groupby('player_id') \
+                        [ncols].transform(lambda x : x.rolling(window=5,min_periods=1).mean())
+
+
+    #7 game moving average
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min'):].columns
+    ndf[[f"{i}_7MA" for i in ncols]] = ndf.groupby('player_id') \
+                        [ncols].transform(lambda x : x.rolling(window=7,min_periods=1).mean())
+
+
+    #10 game moving average
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min'):].columns
+    ndf[[f"{i}_10MA" for i in ncols]] = ndf.groupby('player_id') \
+                        [ncols].transform(lambda x : x.rolling(window=10,min_periods=1).mean())
+
+
+    #moving average + cum calcs.
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min'):].columns
+    ndf[[f"{i}_cum_avg" for i in ncols]] = ndf.groupby('player_id') \
+                        [ncols].transform(lambda x : x.expanding(axis=0).mean())
+
+
+    #cumsum calcs
+    ncols = ndf.iloc[:,ndf.columns.get_loc('min'):ndf.columns.get_loc('fp')+1].columns
+    ndf[[f"{i}_cum_sum" for i in ncols]] = ndf.groupby('player_id') \
+                        [ncols].transform(lambda x : x.expanding(axis=0).sum())
+
+
+    #per min cumsum metrics
+    ncols = ndf.iloc[:,ndf.columns.get_loc('fg_cum_sum'):ndf.columns.get_loc('fp_cum_sum')+1].columns
+    ndf[[f"{i}_per_min" for i in ncols]] = ndf[ncols].transform(lambda x : x/ndf['min_cum_sum'])
+
+    #per cum sum fp %
+    ncols = ndf.iloc[:,ndf.columns.get_loc('fp_fg_cum_sum'):ndf.columns.get_loc('fp_cum_sum')].columns
+    ndf[[f"{i}_%_fp_cum_sum" for i in ncols]] = ndf[ncols].transform(lambda x : x/ndf['fp_cum_sum'])
+
+
+    #make dummy columns
+    ndf = pd.get_dummies(ndf,columns=['pos'])
+
+    #find last start
+    ndf['last_start'] = ndf.groupby('player_id') \
+                ['start'].transform(lambda x : x.shift(1))
+
+
+    ndf['new_start'] = np.where((ndf['start'] == 'Y')
+                & (ndf['last_start'] =='N'), #Identifies the case to apply to
+                1,      #This is the value that is inserted
+                0) 
+
+    return ndf
+
+
+if __name__ == "__main__":
+    raw_data_dir = '~/nba_dfs_value/data/raw/'
+    interim_data_dir= '~/nba_dfs_value/data/interim/'
+    processed_data_dir = '~/nba_dfs_value/data/processed/'
+    ndf = pd.read_pickle(interim_data_dir+"clean_merge.pkl")
+
+    ndf = build_features(ndf)
+    ndf.to_pickle(interim_data_dir+ 'df_features.pkl')
